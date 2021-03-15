87130913d54a96d773d156432e5ef7818c6d6683
Kayla Hunt <78668827+huntress284@users.noreply.github.com>
2021-03-14 22:31:50 -0400

    Create levelManager.cs
    levelManager

---
+++ platform-game/Assets/levelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelManager : MonoBehaviour
+{
+    public int cherryValue;
+    public PlayerMovement gamePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePlayer = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCoins(int numOfCherry)
+    {
+        cherryValue += numOfCherry;
+    }
+}
