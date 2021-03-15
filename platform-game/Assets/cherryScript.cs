d773886dd2588e8eafe04bd3eedc677ebeb44d28
Kayla Hunt <78668827+huntress284@users.noreply.github.com>
2021-03-14 22:31:30 -0400

    Create cherryScript.cs
    cherryScript

---
+++ platform-game/Assets/cherryScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cherryScript : MonoBehaviour
+{
+    private levelManager gameLevelManager;
+    public int cherryScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "player")
+        {
+
+
+            Destroy(gameObject);
+            cherryScore++;
+
+
+            //gameLevelManager.AddCoins(cherryScore);
+            //Destroy(gameObject);
+        }
+    }
+}
